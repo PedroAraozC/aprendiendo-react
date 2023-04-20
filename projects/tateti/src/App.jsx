6a9b5ec8cedafaebdf1146894fab40591ba87b40
Pedro Araoz <pedroaraozc@gmail.com>
2023-04-20 17:11:41 -0300

    Se agregó almacenamiento en local storage de turnos y tablero a ta te ti

--- projects/tateti/src/App.jsx
+++ projects/tateti/src/App.jsx
@@ -9,24 +9,28 @@ import { checkWinnerFrom, checkEndGame } from './logic/board.js';
 import { WinnerModal } from './components/WinnerModal.jsx';
 
 function App() {
+  // si hay una partida guardada
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
 
-  const [board, setBoard] = useState
-    (
-      Array(9).fill(null)
-    )
-
-  const [turn, setTurn] = useState(TURNS.X)
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
   //null no hay ganador, false hay empate
   const [winner, setWinner] = useState(null)
-
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
-  }
 
- 
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+  }
 
   const updateBoard = (index) => {
     //no actualiza el estado si ya hay un valor
@@ -37,7 +41,10 @@ function App() {
     setBoard(newBoard)
     //cambia el turnos
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-    setTurn(newTurn)
+    // guardar partida
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn',newTurn)
+    setTurn(newTurn) // actualizar el estado con el nuevo valor de newTurn
     // revisamos si hay ganador
     const newWinner = checkWinnerFrom(newBoard)
 
